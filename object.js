e379bda6babc72bdaafed9d0744339efed567f8d
arafatrakib1 <msaraib3@gmail.com>
2017-12-29 11:48:38 +0600

    testing object model

---
+++ object.js
@@ -0,0 +1,20 @@
+// object model, that's a power of a OOP language
+function Details(name, institution, roll, home, semester) { // in the bracket put all the argument you need in a object model
+  this.name = name
+  this.institution = institution
+  this.roll = roll
+  this.home = home
+  this.semester = semester
+  this.UpdateSemester = function(){ // this anonymousfuunction will update semester time to time
+    return ++this.semester
+  }
+}
+
+var student1 = new Details("rakib", "NSU", 1721330, "jessore", "1st")
+
+console.log(student1) // full details
+console.log("Name:",student1.name) //read single property
+console.log("Home:",student1.home) //read single property
+console.log("Institution:",student1.institution) //read single property
+console.log("Roll:",student1.roll) //read single property
+console.log("semester:", student1.semester) //read single property
